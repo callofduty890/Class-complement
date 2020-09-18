e4b3b0f8cdd94e28dc8e367161fa3ebb6f8841bb
俊华 蔡 <1032031912@qq.com>
2020-09-18 11:01:57 +0800

    连接数据库

---
+++ 04_连接数据库/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_连接数据库
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //编辑SQL Server数据库连接字符串
+            string conString = @"Server=DESKTOP-CTV4ATU\SQLSERVER;DataBase=TEST_06;Uid=sa;pwd=123456";
+            //创建连接对象
+            SqlConnection conn = new SqlConnection(conString);
+            //打开数据库
+            conn.Open();
+            //判断是否打开数据库
+            if (conn.State==System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection is opened!");
+            }
+            //关闭连接
+            conn.Close();
+            //判断是否关闭数据库
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                Console.WriteLine("Connection is Closed!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
