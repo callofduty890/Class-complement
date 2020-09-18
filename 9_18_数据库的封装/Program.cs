29bc5a0c02348dee301775df279e7fa15cbccd11
俊华 蔡 <1032031912@qq.com>
2020-09-18 08:55:54 +0800

    添加项目文件。

---
+++ 9_18_数据库的封装/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_18_数据库的封装
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
